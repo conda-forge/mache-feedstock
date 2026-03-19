2ad2244ed2f842e1fef8ab05d222e671351659e6
Xylar Asay-Davis <xylarstorm@gmail.com>
2026-03-19 16:47:16 +0100

    Update post-link script copy path in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,5 @@ $PYTHON -m pip install -vv --no-deps .
 
 mkdir -p "$PREFIX"/bin
 POST_LINK="$PREFIX"/bin/.mache-post-link.sh
-cp "$SRC_DIR"/conda/post-link.sh "$POST_LINK"
+cp "$RECIPE_DIR"/post-link.sh "$POST_LINK"
 chmod +x "$POST_LINK"
